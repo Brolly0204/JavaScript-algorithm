97c45614aeb897626560f138d4f5733e6838e47b
L.W.L <102495553@qq.com>
2019-05-06 13:09:16 +0800

    fix: 修复数组中根据重复出现次数排序

--- code/array/repeatTimeSort.js
+++ code/array/repeatTimeSort.js
@@ -6,13 +6,22 @@
 
 const repeatTimeSort = arr => {
   arr.sort((a, b) => a - b)
-  const map = arr.join('').match(/(\d)\1*/g)
-  map.sort((a, b) => a.length - b.length)
-  return map
-    .join('')
-    .split('')
-    .map(Number)
+  let ary = []
+  while (arr.length > 0) {
+    let a = arr[0]
+    let start = arr.indexOf(a)
+    let end = arr.lastIndexOf(a) + 1
+    ary.push(arr.splice(start, end - start))
+    // ary.push(arr.slice(start, end))
+    // arr = arr.splice(end)
+  }
+  ary.sort((a, b) => a.length - b.length)
+  return ary.reduce((prev, cur) => prev.concat(cur), [])
 }
 
-// console.log(repeatTimeSort([1, 2, 1, 2, 1, 3, 4, 5, 4, 5, 5, 2, 2]))
+// [ 12, 13, 13, 11, 11, 11 ]
+console.log(repeatTimeSort([11, 12, 13, 11, 11, 13]))
+// [ 3, 4, 4, 1, 1, 1, 5, 5, 5, 2, 2, 2, 2 ]
+console.log(repeatTimeSort([1, 2, 1, 2, 1, 3, 4, 5, 4, 5, 5, 2, 2]))
+
 export default repeatTimeSort
--- test/array/repeatTimeSort.test.js
+++ test/array/repeatTimeSort.test.js
@@ -2,6 +2,9 @@ import repeatTimeSort from '../../code/array/repeatTimeSort'
 
 const input = [1, 2, 1, 2, 1, 3, 4, 5, 4, 5, 5, 2, 2]
 const output = [3, 4, 4, 1, 1, 1, 5, 5, 5, 2, 2, 2, 2]
+const input2 = [11, 12, 13, 11, 11, 13]
+const output2 = [12, 13, 13, 11, 11, 11]
 test('repeatTimeSort', () => {
   expect(repeatTimeSort(input)).toEqual(output)
+  expect(repeatTimeSort(input2)).toEqual(output2)
 })
